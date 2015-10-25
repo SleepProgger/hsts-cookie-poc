cc459dbc5ba8cda2bdfc03b5189df2a667d1609d
SleepProgger <SleepProgger@users.noreply.github.com>
2015-10-25 05:50:32 +0100

    Set now also returns the correct value

--- hsts_cookie.php
+++ hsts_cookie.php
@@ -5,6 +5,8 @@ $is_ssl = !empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['
 if(isset($_GET['SET'])){
 	if($is_ssl){
 		header('Strict-Transport-Security: max-age=31536000');
+		header('Content-type: image/png');
+        	echo base64_decode('iVBORw0KGgoAAAANSUhEUgAAAAgAAAAJCAIAAACAMfp5AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAYSURBVBhXY/z//z8DNsAEpTHAkJJgYAAAo0sDD8axyJQAAAAASUVORK5CYII=');
 	}else{
 		$redirect = "https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
 		header("Location: $redirect");
